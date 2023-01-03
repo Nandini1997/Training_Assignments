8ccc4df124dc1695b0c7f98baa78116f816c0ccc
NandiniSrinivas <nandininandy817@gmail.com>
2023-01-03 17:49:40 +0530

    driver code for spark1 assignment

---
+++ Spark_1/code/driver_code.py
@@ -0,0 +1,34 @@
+from Spark_1.code._utils_code import *
+
+#creating spark session
+spark = sparkSession()
+
+#creating transaction dataframe
+trans_df = trasactiondf(spark)
+
+#creating user dataframe
+user_df = userdf(spark)
+
+#joining two df
+print("Transaction Scema")
+trans_df.printSchema()
+print("User Schema")
+user_df.printSchema()
+trans_user_join=joining_trans_user(trans_df, user_df)
+print("Schema after joining df's")
+trans_user_join.printSchema()
+
+#Count of unique locations where each product is sold
+print("Count of unique locations where each product is sold")
+countLocation = countDistinctLocation(trans_user_join, "location ")
+countLocation.show()
+
+#Find out products bought by each user.
+print("Find out products bought by each user")
+user_product = prod_by_users(trans_user_join, "userid", "product_description")
+user_product.show()
+
+#Total spending done by each user on each product
+print("Total spending done by each user on each product")
+spendings = sum_spending_by_prod(trans_user_join, "userid", "product_id", "price")
+spendings.show()
