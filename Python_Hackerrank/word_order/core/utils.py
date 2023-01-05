59d8bfe97649bc27f82c596c08b08a1e0b173bf2
NandiniSrinivas <nandininandy817@gmail.com>
2023-01-05 20:39:13 +0530

    utils file of python word order for hackerrank problem

--- Python_Hackerrank/word_order/core/utils.py
+++ Python_Hackerrank/word_order/core/utils.py
@@ -2,9 +2,10 @@ from collections import Counter
 
 def word_order_find(count,arr_input):
     arr=[]
+    output = ""
     for i in range(count):
         arr.append(arr_input[i])
-    print(len(set(arr)))
     my_dict=Counter(arr)
     for i in my_dict.values():
-        print(i,end=" ")
+        output=output+str(i)+" "
+    return output+str(len(set(arr)))
