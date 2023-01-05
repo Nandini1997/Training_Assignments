fd28ce5e018e4486cabf33ea478a87e3c18026fd
NandiniSrinivas <nandininandy817@gmail.com>
2023-01-05 20:36:08 +0530

    utils file for finding linear algebra

--- Python_Hackerrank/np_linear_algebra/core/utils.py
+++ Python_Hackerrank/np_linear_algebra/core/utils.py
@@ -1,10 +1,10 @@
 import numpy
 
 def np_linear_algebra(input1,input2):
-    num = int(input1)
+    num = input1
     arr_list = []
     for i in range(num):
-        arr_list.append(input2.split())
+        arr_list.append(input2[i])
 
     input_list = numpy.array(arr_list, dtype=float)
-    print(round(numpy.linalg.det(input_list), 2))
+    return (round(numpy.linalg.det(input_list), 2))
