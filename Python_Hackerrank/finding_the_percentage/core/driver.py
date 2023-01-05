95f1a49c7e6f74f50d4e756951e060f79a6530b7
NandiniSrinivas <nandininandy817@gmail.com>
2023-01-05 20:32:31 +0530

    driver file of finding the percentage for hackerrank problem

--- Python_Hackerrank/finding_the_percentage/core/driver.py
+++ Python_Hackerrank/finding_the_percentage/core/driver.py
@@ -2,7 +2,7 @@ from Python_Hackerrank.finding_the_percentage.core.utils import *
 
 
 
-sum = finding_percentage(name="Nandini", student_mark={"Nandini":[23,4,55,65], "Divya":[34,56,32]})
+sum = round(finding_percentage(name="Divya", student_mark={"Nandini":[23,4,55,65], "Divya":[34,56,32]}),3)
 print(sum)
 
 
