13879893c191134b110819575c0e78f147fd7b1f
NandiniSrinivas <nandininandy817@gmail.com>
2023-01-05 20:31:30 +0530

    core file for finding the day from date

--- Python_Hackerrank/calender_module/core/core.py
+++ Python_Hackerrank/calender_module/core/core.py
@@ -3,4 +3,4 @@ import calendar
 
 def findDay(input):
     datetime_is = datetime.strptime(input, '%m %d %Y')
-    print(calendar.day_name[datetime_is.weekday()].upper())
+    return (calendar.day_name[datetime_is.weekday()].upper())
