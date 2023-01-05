ad75d8a564f075ff8199cb4a0736fbd498c3e1b2
NandiniSrinivas <nandininandy817@gmail.com>
2023-01-05 20:37:35 +0530

    driver file of python mutation for hackerrank problem

--- Python_Hackerrank/python_mutation/core/driver.py
+++ Python_Hackerrank/python_mutation/core/driver.py
@@ -1,2 +1,6 @@
-def mutate_string(string, position, character):
-    return string[:position]+character+string[position+1:]
+from Python_Hackerrank.python_mutation.core.utils import *
+if __name__ == '__main__':
+    s = "nandinisrinivas"
+    i, c = 5,'k'
+    s_new = mutate_string(s, i, c)
+    print(s_new)
