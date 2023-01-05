a05fe6c989704aa321a3a37e673425a86af71915
NandiniSrinivas <nandininandy817@gmail.com>
2023-01-05 20:35:19 +0530

    driver file of merge the tools for hackerrank problem

--- Python_Hackerrank/merge_the_tools/core/driver.py
+++ Python_Hackerrank/merge_the_tools/core/driver.py
@@ -1,4 +1,4 @@
 from Python_Hackerrank.merge_the_tools.core.utils import *
 
-string, k = "AABCAAADA", 3
-merge_the_tools(string, k)
+string, k = "AABCAAADAHGAUIAAAKLA", 3
+print(merge_the_tools(string, k))
