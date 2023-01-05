5e8763fecb237602460f7d69c2653842218b1df7
NandiniSrinivas <nandininandy817@gmail.com>
2023-01-05 20:37:06 +0530

    driver file of python delta for hackerrank problem

--- Python_Hackerrank/python_delta_time/core/driver.py
+++ Python_Hackerrank/python_delta_time/core/driver.py
@@ -1,6 +1,6 @@
 from Python_Hackerrank.python_delta_time.core.utils import *
 
-t1 = input()
-t2 = input()
+t1 = "Sun 10 May 2015 13:54:36 -0700"
+t2 = "Sun 10 May 2015 13:54:36 -0000"
 delta = time_delta(t1, t2)
 print(delta)
