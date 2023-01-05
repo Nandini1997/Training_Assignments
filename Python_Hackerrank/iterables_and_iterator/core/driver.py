f72cd5efea5a8715812b1ae19be8334edad5b281
NandiniSrinivas <nandininandy817@gmail.com>
2023-01-05 20:34:46 +0530

    driver file of iterables and iterators for hackerrank problem

--- Python_Hackerrank/iterables_and_iterator/core/driver.py
+++ Python_Hackerrank/iterables_and_iterator/core/driver.py
@@ -2,4 +2,4 @@ from Python_Hackerrank.iterables_and_iterator.core.utils import *
 number=4
 string="a a c d"
 slice_index=2
-find_iterator_anditerables(number, string, slice_index)
+print(find_iterator_anditerables(number, string, slice_index))
