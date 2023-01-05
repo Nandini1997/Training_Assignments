f0ee5aceb3c08fd09a05efa877bd955e7654ecbe
NandiniSrinivas <nandininandy817@gmail.com>
2023-01-05 20:37:49 +0530

    utils file of python mutation for hackerrank problem

--- Python_Hackerrank/python_mutation/core/utils.py
+++ Python_Hackerrank/python_mutation/core/utils.py
@@ -1,6 +1,2 @@
-from Python_Hackerrank.python_mutation.core.driver import *
-if __name__ == '__main__':
-    s = input()
-    i, c = input().split()
-    s_new = mutate_string(s, int(i), c)
-    print(s_new)
+def mutate_string(string, position, character):
+    return string[:position] + character + string[position + 1:]
