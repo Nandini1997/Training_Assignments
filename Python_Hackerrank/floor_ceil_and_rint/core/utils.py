72294f760f5cdd5ad311ba3371ac30fb2714bda0
NandiniSrinivas <nandininandy817@gmail.com>
2023-01-05 20:33:54 +0530

    utils file for finding floor, ceil and rint

--- Python_Hackerrank/floor_ceil_and_rint/core/utils.py
+++ Python_Hackerrank/floor_ceil_and_rint/core/utils.py
@@ -2,8 +2,10 @@ import numpy
 numpy.set_printoptions(legacy='1.13')
 
 def find_floor_ceil_rint(input):
-    my_list = list(map(float, input.split()))
-    print(numpy.floor(my_list))
-    print(numpy.ceil(my_list))
-    print(numpy.rint(my_list))
+    my_list = input
+    output_list = []
+    output_list.append(numpy.floor(my_list))
+    output_list.append(numpy.ceil(my_list))
+    output_list.append(numpy.rint(my_list))
+    return output_list
 
