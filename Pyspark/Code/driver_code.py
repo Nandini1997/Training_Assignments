826ddbe3cd7c16db3fb5e31492c1ae7b65560fa9
Standard <nandininandy817@gmail.com>
2023-01-13 20:29:10 +0530

    pyspark driver file python

--- Pyspark/Code/driver_code.py
+++ Pyspark/Code/driver_code.py
@@ -5,15 +5,12 @@ spark = createSparkSession()
 
 #creating product details df
 prod_details_df=read_prod_df(spark, "../../resource/Product.csv")
-prod_details_df.show()
 
 # converting milliseconds to time stamp format and converting to date format only
 df_with_timestamp = to_timestamp_con(prod_details_df, "Issue_Date_timestamp", "IssueDate", "Issue_Date_Form")
 
 #Fill null with nothing
 df_prod_details_res = fill_null_na(df_with_timestamp)
-print("Printing final df after transformations")
-df_prod_details_res.show()
 
 #creating source df
 source_df = createSource(spark, "../../resource/Source.csv")
@@ -23,8 +20,6 @@ df_source_col_name = to_snake_case(source_df)
 
 #Converting start_time to unix time seconds and adding it as new column
 df_source_res = to_unix_timestamp(df_source_col_name,"epoch_seconds", "start_time")
-print("Final source df after transformation")
-df_source_res.show()
 
 #Combine both the file and return the country EN
 df_final_output = to_join_prod_source(df_prod_details_res,df_source_res, "EN")
